4e7816df7c84b55ab30b445f30578b5f4aedff78
Tezsla Richard <tezsla.richard@BOLYAI.LOCAL>
2024-10-04 15:33:05 +0200

    get values from form

--- main.js
+++ main.js
@@ -92,7 +92,21 @@ for(let pers of array){
         td_hazas.innerHTML = "nem"
     }
 }
-    
+const form = document.getElementById("form");
+form.addEventListener("submit", function(e){
+    const lastname = document.getElementById("lastname")
+    const firstname1 = document.getElementById("firstname1")
+    const firstname2 = document.getElementById("firstname2")
+    const married = document.getElementById("married")
+    const pet = document.getElementById("pet")
+    const lastnamevalue = lastname.value;
+    const firstname1value = firstname1.value;
+    const firstname2value = firstname2.value;
+    const marriedvalue = married.checked;
+    const petvalue = pet.value;
+})
+
+ 
 
  
  
