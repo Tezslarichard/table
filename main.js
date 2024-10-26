99c7c1e820dd677aaced97fc989eabf315e29199
Tezslarichard <tezslarichard@gmail.com>
2024-10-26 20:40:56 +0200

    előre törés,09.27. óra készen van

--- main.js
+++ main.js
@@ -21,18 +21,52 @@ const array = [
 ]
 const table = document.createElement("table");
 const thead = document.createElement("thead");
-const tr = document.createElement("tr")
-const td = document.createElement("td")
-const th_firstname1 = document.createElement("firstname1");
-const th_lastname = document.createElement("lastname");
+const tbody = document.createElement("tbody")
+const tr = document.createElement("tr");
+const td = document.createElement("td");
+const th_firstname1 = document.createElement("th");
+const th_lastname = document.createElement("th");
 
-document.body.appendChild("table");
-table.appendChild("thead")
-thead.appendChild("tr")
-tr.appendChild("td")
+document.body.appendChild(table);
+table.appendChild(thead);
+thead.appendChild(tr);
+
+th_firstname1.innerHTML = "Keresztnév";
+th_lastname.innerHTML = "Vezetéknév";
+tr.appendChild(th_lastname);
+tr.appendChild(th_firstname1);
+table.appendChild(tbody);
+th_firstname1.colSpan = 2;
 
 
-th_firstname1.innerHTML = "Keresztnév"
-th_lastname.innerHTML = "Vezetéknév"
-tr.appendChild(th_firstname1)
-tr.appendChild(th_lastname)
+for(const pers of array){
+    const tr_body = document.createElement('tr');
+    tbody.appendChild(tr_body);
+
+    const td_firstname1 = document.createElement('td');
+    td_firstname1.innerHTML = pers.firstname1;
+    
+
+    const td_lastname = document.createElement('td')
+    td_lastname.innerHTML = pers.lastname;
+    tr_body.appendChild(td_lastname)
+    tr_body.appendChild(td_firstname1)
+    
+    
+
+
+
+    
+    if(pers.firstname2 == undefined){
+        td_firstname1.colSpan = 2 ;
+    }
+    else{
+        const td_firstname2 = document.createElement('td');
+        td_firstname2.innerHTML = pers.firstname2;
+        tr_body.appendChild(td_firstname2);
+    }
+    
+}
+
+
+    
