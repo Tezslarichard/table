4bf275a996de7bad3a2f043a0aaaee70767e00e0
Tezslarichard <tezslarichard@gmail.com>
2024-10-30 13:29:12 +0100

    kész 10.11

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ rendertable();
 const form = document.getElementById('form');
 
 form.addEventListener('submit',function(e){
-    tbody.innerHTML = "";
+    
     const firstname1 = document.getElementById('firstname1');
     const firstname2 = document.getElementById('firstname2');
     const lastname = document.getElementById('lastname');
@@ -79,7 +79,10 @@ form.addEventListener('submit',function(e){
     if(firstname2value == "" ){
         firstname2value = undefined;
     }
-    if(validatefields(lastname,firstname1,pet)){
+
+
+    
+        if(validatefields(lastname,firstname1,pet)){
         const newpers = {
             lastname: lastnamevalue,
             firstname1: firstname1value,
@@ -95,6 +98,7 @@ form.addEventListener('submit',function(e){
 
 
 function rendertable(){
+    tbody.innerHTML = "";
     for (const pers of array) {
     const tr_body = document.createElement('tr');
 
@@ -137,3 +141,30 @@ function rendertable(){
     }
 }
 
+function validatefields(lasthtml,firsthtml,pethtml){
+    let result = true
+    if(lasthtml.value === ""){
+        const parent_element = lasthtml.parentElement;
+        const error = parent_element.querySelector('.error');
+        error.innerHTML = "Szép munka, de ki kell tölteni";
+        
+        result = false;
+
+    }
+    if(firsthtml.value === ""){
+        const parent_element = firsthtml.parentElement;
+        const error = parent_element.querySelector('.error');
+        error.innerHTML = "Szép munka, de ki kell tölteni";
+        result = false;
+        
+    }
+    if(pet.value === ""){
+        const parent_element = pethtml.parentElement;
+        const error = parent_element.querySelector('.error');
+        error.innerHTML = "Szép munka, de ki kell tölteni";
+        result = false;
+        
+    }
+
+    return result;
+}
